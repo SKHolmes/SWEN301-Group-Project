7965804990a1d1284f96e2fda10523441a5a967f
SKHolmes <samualkholmes@gmail.com>
2016-06-21 23:47:30 +1200

    added graph library

---
+++ src/routes/Djikstra.java
@@ -0,0 +1,12 @@
+package routes;
+import org.jgrapht.graph.DefaultDirectedWeightedGraph;
+
+public class Djikstra {
+	
+	private DefaultDirectedWeightedGraph graph;
+	
+	public Djikstra(){
+		
+	}
+
+}
